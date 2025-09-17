31a8ff539e6bfc39c8b3c1ebe0285598bbcd2ad1
QaJean <jcleite65@gmail.com>
2025-09-17 20:20:17 -0300

    Deploy site com 404.html

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/online-utility-suite/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/imc" element={<ImcPage />} />
