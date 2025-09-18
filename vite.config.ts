e9262edfe94ad65e88732fad73345658d97c00d9
QA Analyst Jean Carlos Leite <jcleite65@gmail.com>
2025-09-18 14:05:00 -0300

    Update vite.config.ts
    caminho diferente para SPA

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // caminho relativo
+  base: "/", // caminho relativo
   server: {
     host: "::",
     port: 8080,
